6e01718e095fbaa2e65276f1cc012b92a89b4bf4
MinaSameh <mina.sameh@blink22.com>
2018-05-01 12:22:50 +0200

    Define Routes

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,10 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import PageNotFound from './common/PageNotFound'
 import Home from './landing'
+import Students from './student'
+import Courses from './courses'
+import manageCourses from './manageCourses'
+
 import './app.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
@@ -12,6 +16,9 @@ const App = () => {
         <div className={'container'}>
           <Switch>
             <Route exact path="/" component={Home} />
+            <Route exact path="/students" component={Students} />
+            <Route exact path="/courses" component={Courses} />
+            <Route exact path="/manageCourses" component={manageCourses} />
             <Route component={PageNotFound} />
           </Switch>
         </div>
---
+++ src/components/manageCourses/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const manageCourses = () => (
+  <div>
+    <h1>manageCourses</h1>
+  </div>
+)
+
+export default manageCourses
